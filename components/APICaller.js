8ae38c9717d5c00f5d69ffb6b5b3523d38c87df4
wenkai <twk97@hotmail.com>
2021-10-06 16:35:57 +0800

    backend call to uploadPDF works
    123

--- components/APICaller.js
+++ components/APICaller.js
@@ -1,122 +1,120 @@
 import axios from 'axios';
 
-const url = "https://sleepy-plateau-79000.herokuapp.com/api/";
+const url = 'https://sleepy-plateau-79000.herokuapp.com/api/';
 
 //========== APIS for Login ==========//
 
-export const loginUser = async (googleLoginDetails) => {
-    try {
-        const { data } = await axios.post(url + "users/login", googleLoginDetails);
-        return data;
-    }
-    catch (error) {
-        console.log(error.message);
-    }
-} 
+export const loginUser = async googleLoginDetails => {
+  try {
+    const {data} = await axios.post(url + 'users/login', googleLoginDetails);
+    return data;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
 //========== APIS for PDFs ==========//
 
-export const uploadPDF = async (PDFobject) => {
-    try {
-        const { data } = await axios.post(url + "upload", PDFobject);
-        return data;
-    }
-    catch (error) {
-        console.log(error.message);
-    }
-}
+export const uploadPDF = async PDFobject => {
+  const {data} = await axios.post(url + 'upload', PDFobject);
+  return data;
+  // try {
+  //     const { data } = await axios.post(url + "upload", PDFobject);
+  //     return data;
+  // }
+  // catch (error) {
+  //     console.log(error.message);
+  // }
+};
 
 //========== APIS for Audiobooks ==========//
 
-export const getAudiobookTitles = async (userID) => {
-    try {
-        const { data } = await axios.get(url + "audiobooks/titles/?userid=" + userID);
-        return data;
-    }
-    catch (error) {
-        console.log(error.message);
-    }
-}
+export const getAudiobookTitles = async userID => {
+  try {
+    const {data} = await axios.get(url + 'audiobooks/titles/?userid=' + userID);
+    return data;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
-export const getAudiobookText = async (bookID) => {
-    try {
-        const { data } = await axios.get(url + "audiobooks/" + bookID);
-        return data;
-    }
-    catch (error) {
-        console.log(error.message);
-    }
-}
+export const getAudiobookText = async bookID => {
+  try {
+    const {data} = await axios.get(url + 'audiobooks/' + bookID);
+    return data;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
 export const getAudiobookProgress = async (bookID, userID) => {
-    try {
-        const { data } = await axios.get(url + "audiobooks/" + bookID + "/progress/?userid=" + userID);
-        return data;
-    }
-    catch (error) {
-        console.log(error.message);
-    }
-}
+  try {
+    const {data} = await axios.get(
+      url + 'audiobooks/' + bookID + '/progress/?userid=' + userID,
+    );
+    return data;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
 export const updateAudiobookProgress = async (bookID, userID, currentPage) => {
-    try {
-        const { data } = await axios.put(url + "audiobooks/" + bookID + "/progress/?userid=" + userID, 
-                        { "current_page": currentPage});
-        return data;
-    }
-    catch (error) {
-        console.log(error.message);
-    }
-}
+  try {
+    const {data} = await axios.put(
+      url + 'audiobooks/' + bookID + '/progress/?userid=' + userID,
+      {current_page: currentPage},
+    );
+    return data;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
-export const deleteAudiobook = async (bookID) => {
-    try {
-        const { data } = await axios.delete(url + "audiobooks/" + bookID);
-        return data;
-    }
-    catch (error) {
-        console.log(error.message);
-    }
-}
+export const deleteAudiobook = async bookID => {
+  try {
+    const {data} = await axios.delete(url + 'audiobooks/' + bookID);
+    return data;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
 export const shareAudiobook = async (userToShareTo, bookID, emailToShareTo) => {
-    try {
-        const { data } = await axios.post(url + "users/share", 
-                        { "user_id": userToShareTo,
-                          "book_id": bookID,
-                          "email": emailToShareTo });
-        return data;
-    }
-    catch (error) {
-        console.log(error.message);
-    } 
-}
+  try {
+    const {data} = await axios.post(url + 'users/share', {
+      user_id: userToShareTo,
+      book_id: bookID,
+      email: emailToShareTo,
+    });
+    return data;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
 //========== APIS for Bookmarks ==========//
 
 export const addBookmark = async (userID, bookID, bookmarkName, page) => {
-    try {
-        const { data } = await axios.put(url + "users/bookmark", 
-                      { "user_id": userID,
-                        "book_id": bookID,
-                        "name": bookmarkName,
-                        "page": page });
-        return data;
-    }
-    catch (error) {
-        console.log(error.message);
-    }
-}
+  try {
+    const {data} = await axios.put(url + 'users/bookmark', {
+      user_id: userID,
+      book_id: bookID,
+      name: bookmarkName,
+      page: page,
+    });
+    return data;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
 export const removeBookmark = async (userID, bookID, bookmarkID) => {
-    try {
-        const { data } = await axios.delete(url + "users/bookmark", 
-                      { data : { "user_id": userID,
-                                 "book_id": bookID,
-                                 "bookmark_id": bookmarkID }});
-        return data;
-    }
-    catch (error) {
-        console.log(error.message);
-    }
-}
+  try {
+    const {data} = await axios.delete(url + 'users/bookmark', {
+      data: {user_id: userID, book_id: bookID, bookmark_id: bookmarkID},
+    });
+    return data;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
--- components/UploadButton.js
+++ components/UploadButton.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useContext} from 'react';
 import {Button, View, Text, TouchableHighlight} from 'react-native';
 import {StyleSheet} from 'react-native';
 
@@ -7,16 +7,15 @@ import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
 import {faUserCircle} from '@fortawesome/free-solid-svg-icons';
 import {faFolderOpen} from '@fortawesome/free-solid-svg-icons';
 
-import { COLORS, FONTS } from '../constants/theme';
+import {COLORS, FONTS} from '../constants/theme';
+import {UserContext} from '../App';
+import {uploadPDF, getAudiobookTitles} from './APICaller';
 
 export default function UploadButton() {
   const [singleFile, setSingleFile] = useState(null);
+  const {setSignedIn, userInfo, setUserInfo} = useContext(UserContext);
 
-  //   const selectFile = () => {
-  //     setSingleFile(5);
-  //   };
-
-  const log = async () => {
+  const uploadFn = async () => {
     if (singleFile != null) {
       //console.log(singleFile); singleFile obj = {fileCopyUri, name, size, type, uri}
       console.log('uploading file');
@@ -26,20 +25,16 @@ export default function UploadButton() {
       // fetch(url, [options] )
       // options = { method: "POST", body: formData }
       const data = new FormData();
-      data.append('file_attachment', singleFile);
-
-      // testing fetch
-      //   const url = 'https://httpbin.org/post'; // 'http://localhost:3000';
-      //   let res = await fetch(url, {
-      //     method: 'POST',
-      //     body: data,
-      //   });
-      //   let responseJson = await res.json();
-      //   console.log(responseJson);
-      //   alert(responseJson);
-      // end of fetch test
+      data.append('id', userInfo.user.id);
+      data.append('pdf', singleFile);
+      uploadPDF(data)
+        .then(() => {
+          alert('file uploaded');
+          setSingleFile(null);
+        })
+        .catch(e => alert('did not upload file successfully', e));
     } else {
-      alert('Please select a File first');
+      alert('Please select a File');
     }
   };
 
@@ -90,28 +85,21 @@ export default function UploadButton() {
       // end of fetch test
     */
 
-  const APICall = async () => {
-    console.log('testing API calls');
-    const url = 'https://randomuser.me/api/';
-    let res = await fetch(url, {
-      method: 'GET',
-    });
-    let responseJson = await res.json();
-    console.log(responseJson);
-    alert(responseJson.results[0].email);
+  const testAPI = async userId => {
+    getAudiobookTitles(userId)
+      .then(data => alert(data[0]['_id']))
+      .catch(e => console.log('testapi, error', e));
   };
-
   return (
     <View style={styles.container}>
-      <Text style={{fontWeight: 'bold', paddingTop: 10,...FONTS.h2}}> {'Upload Files'} </Text>
+      <Text style={{fontWeight: 'bold', paddingTop: 10, ...FONTS.h2}}>
+        {' '}
+        {'Upload PDF\n -Make sure PDF is a clear scan\n -legally obtained'}{' '}
+      </Text>
       <TouchableHighlight onPress={selectFile}>
         <View style={styles.selectFile}>
-          <FontAwesomeIcon
-            icon={faFolderOpen}
-            size={100}
-            color={'orange'}
-          />
-          <Text>{'Choose files here...'}</Text>
+          <FontAwesomeIcon icon={faFolderOpen} size={100} color={'orange'} />
+          <Text>{'Choose file here...'}</Text>
         </View>
       </TouchableHighlight>
 
@@ -127,7 +115,8 @@ export default function UploadButton() {
       </View>
 
       <View style={styles.uploadBtn}>
-        <Button title="Upload PDF" onPress={log} />
+        <Button title="Upload PDF" onPress={uploadFn} />
+        {/* <Button title="testAPI" onPress={() => testAPI(userInfo.user.id)} /> */}
       </View>
     </View>
   );
