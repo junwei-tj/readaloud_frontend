dd2d4ef4ced37542474d5bc017c417c3f2492331
Jun Wei <tohjunwei.tj@gmail.com>
2021-10-06 16:17:52 +0800

    finished core playback screen logic, excluding API integration

---
+++ components/Bookmarks.js
@@ -0,0 +1,140 @@
+import React, { useState, useRef } from 'react';
+import { StyleSheet, View, Text, FlatList, Pressable, TextInput, Alert } from 'react-native';
+import { Dimensions } from 'react-native';
+
+import { COLORS, SIZES, FONTS } from '../constants/theme';
+
+const windowHeight = Dimensions.get('window').height;
+
+const styles = StyleSheet.create({
+  container: {
+    height: 0.65*windowHeight,
+    width: '100%',
+    backgroundColor: COLORS.offblack,
+    borderTopColor: COLORS.saffron,
+    borderTopWidth: 1,
+  },
+  bookmarkContainer: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    borderBottomWidth: 1,
+    borderBottomColor: COLORS.saffron,
+  },
+  bookmarkText: {
+    ...FONTS.h2,
+    color: COLORS.white,
+    padding: 16,
+  },
+  addRow: {
+    flexDirection: 'row',
+    width: '100%',
+  },
+  addBookmarkInput: {
+    width: '90%',
+    backgroundColor: 'dimgray',
+    color: COLORS.white,
+    ...FONTS.body3,
+    paddingHorizontal: 8,
+  },
+  inputError: {
+    borderColor: 'crimson',
+    borderWidth: 1,
+  },
+  addBookmarkButton: {
+    width: '10%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: COLORS.saffron,
+  },
+  plusText: {
+    ...FONTS.h1,
+    color: COLORS.offblack,
+  },
+});
+
+// component to display in FlatList
+const Bookmark = ({ name, page }) => {
+  return (
+    <View style={styles.bookmarkContainer}>
+      <Text style={styles.bookmarkText}>{name}</Text>
+      <Text style={styles.bookmarkText}>{page}</Text>
+    </View>
+  )
+}
+
+// main component to render
+export const Bookmarks = ({ bookmarks, onBookmarkPressed, addNewBookmark, removeOldBookmark }) => {  
+  const [newBookmarkName, setNewBookmarkName] = useState("");
+  const [addBookmarkError, setAddBookmarkError] = useState(false);
+  const textInputRef = useRef(null);
+
+  // item to render in a FlatList
+  const renderItem = ({ item }) => (
+    <Pressable 
+      onPress={() => onBookmarkPressed(item.page)} 
+      onLongPress={() => onBookmarkLongPress(item._id, item.name)}
+      android_ripple={{color: 'dimgray'}}
+    >
+      <Bookmark name={item.name} page={item.page} />
+    </Pressable>
+  )
+
+  // function to add bookmark
+  const onAddPressed = () => {
+    if (newBookmarkName !== "") {
+      addNewBookmark(newBookmarkName);
+      setAddBookmarkError(false);
+      textInputRef.current.clear();
+    } else {
+      setAddBookmarkError(true);
+    }
+  }
+
+  // function to remove bookmark
+  const onBookmarkLongPress = (bookmarkID, bookmarkName) => {
+    Alert.alert(
+      bookmarkName, // alert title
+      "Are you sure you want to delete this bookmark?", // alert message
+      [ // array of buttons
+        {
+          text: "Cancel",
+          style: "cancel"
+        },
+        {
+          text: "Delete",
+          onPress: () => removeOldBookmark(bookmarkID),
+          style: "default",
+        }
+      ],
+      { cancelable: true }, // options (android only)
+    );
+  }
+
+  return (
+    <View style={styles.container}>
+      <FlatList 
+        data={bookmarks}
+        renderItem={renderItem}
+        keyExtractor={item => item._id}
+      />
+      <View style={styles.addRow}>
+        <TextInput 
+          ref={textInputRef}
+          style={[ styles.addBookmarkInput, addBookmarkError ? styles.inputError : {} ]} 
+          placeholder="Bookmark this page"
+          placeholderTextColor="darkgray"
+          maxLength={64}
+          onChangeText={setNewBookmarkName}
+        />
+        <Pressable 
+          onPress={onAddPressed} 
+          android_ripple={{color: 'dimgray'}} 
+          style={styles.addBookmarkButton}
+        >
+          <Text style={styles.plusText}>+</Text>
+        </Pressable>
+      </View>
+    </View>
+  )
+}
--- screens/PlayAudioScreen.js
+++ screens/PlayAudioScreen.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect, useRef, useContext } from 'react';
 import {
   SafeAreaView,
   StatusBar,
@@ -8,14 +8,24 @@ import {
   StyleSheet,
   ScrollView,
 } from 'react-native';
-import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
-import {
-  faChevronCircleLeft,
-  faPlayCircle,
-  faBookmark,
-} from '@fortawesome/free-solid-svg-icons';
-import Slider from '@react-native-community/slider';
-import {COLORS, SIZES, FONTS} from '../constants/theme';
+import { Dimensions, BackHandler } from 'react-native';
+
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faChevronCircleLeft, faPlayCircle, faPauseCircle, faStopCircle, faBookmark } from '@fortawesome/free-solid-svg-icons';
+import Tts from 'react-native-tts';
+import HighlightText from '@sanar/react-native-highlight-text';
+import SlidingUpPanel from 'rn-sliding-up-panel';
+
+import { COLORS, SIZES, FONTS } from '../constants/theme';
+import { book } from '../testBeeMovie'; // REMOVE AFTER INCORPORATING API
+import { progress } from '../testBookProgress'; // REMOVE AFTER INCORPORATING API
+import { Bookmarks } from '../components/Bookmarks';
+import { UserContext } from '../App';
+import { addBookmark, removeBookmark } from './APICaller';
+
+Tts.setDucking(true); // Enable lowering other applications output level while speaking (also referred to as "ducking").
+const sentenceRegex = /[^.?!]+[.!?]+[\])'"`’”]*|.+$/g; // used to split text into sentences
+const windowHeight = Dimensions.get('window').height;
 
 const styles = StyleSheet.create({
   container: {
@@ -29,12 +39,11 @@ const styles = StyleSheet.create({
     paddingHorizontal: 24,
     justifyContent: 'space-between',
     flexDirection: 'row',
-    alignItems: 'center',
+    alignItems: 'center',        
     backgroundColor: COLORS.saffron,
   },
   backIcon: {
     borderRadius: 16,
-    overflow: 'hidden',
     marginRight: 16,
   },
   // text reader
@@ -60,7 +69,7 @@ const styles = StyleSheet.create({
     marginTop: 8,
   },
   slider: {
-    width: '90%',
+    width: '90%', 
     height: 40,
   },
   controlBar: {
@@ -68,128 +77,213 @@ const styles = StyleSheet.create({
     justifyContent: 'space-evenly',
     alignItems: 'center',
     width: '80%',
+    marginTop: 16,
   },
   controlIcons: {
-    width: 64,
-    height: 56,
     borderRadius: 24,
-    // backgroundColor: 'white',
-    justifyContent: 'center',
-    alignItems: 'center',
+    paddingHorizontal: 16,
   },
 });
 
-export default function PlayAudioScreen({navigation}) {
-  const [sliderValue, setSliderValue] = useState(0); // to change to user's last saved position
-  const [maxSliderValue, setMaxSliderValue] = useState(5);
-  const [isPlaying, setIsPlaying] = useState(false);
-  const [intervalObj, setIntervalObj] = useState(null);
+export default function PlayAudioScreen({ navigation, route }) {
+  const [isPlaying, setIsPlaying] = useState(false);    
+  const [bookmarks, setBookmarks] = useState(progress.bookmarks);
+  const [page, setPage] = useState({
+    sentenceNum: 30,
+    pageText: book[0].page[progress.currentPage].body,
+    sentences: book[0].page[progress.currentPage].body.match(sentenceRegex),
+    pageNum: progress.currentPage,
+  });
 
-  const currPageNum = 5; // temp
+  const { userInfo } = useContext(UserContext);
 
-  // function to increment slider position every second
-  // const updateSlider
+  const panelRef = useRef(null);
+  const scrollRef = useRef(null);
 
-  const onPlayPressed = () => {
-    if (!isPlaying) {
-      if (sliderValue === maxSliderValue) {
-        // restart audio
-        setSliderValue(0);
+  // overwrite Android's default back behaviour
+  useEffect(() => { 
+    BackHandler.addEventListener('hardwareBackPress', () => {
+      exitScreen();
+      return true; // other back actions (including system default) will not execute
+    });
+  }, []);
+
+  // function to handle exiting playback screen
+  // will need to stop TTS and save user progress
+  const exitScreen = () => { 
+      if (isPlaying) {
+          setIsPlaying(false);
+          Tts.stop();
       }
-      setIntervalObj(
-        setInterval(() => {
-          setSliderValue(prevValue => prevValue + 1);
-        }, 1000),
-      );
+      navigation.goBack();
+      // TODO: save user progress
+  }
+
+  // eventlistener handler to highlight+play next sentence, and switch to next page if end of page is reached
+  const advance = () => { 
+    if (page.sentences[page.sentenceNum+1] !== undefined) {
+      // go to next sentence            
+      setPage(prev => {
+        Tts.speak(prev.sentences[prev.sentenceNum+1].trim());
+        return ({
+          ...prev,
+          sentenceNum: prev.sentenceNum+1,
+        })
+      });
     } else {
-      clearInterval(intervalObj);
-      console.log('stopped!');
+      // advance to next page
+      setPage(prev => {
+        const newSentences = book[0].page[prev.pageNum+1].body.match(sentenceRegex);
+        Tts.speak(newSentences[0].trim());
+        return ({
+          sentenceNum: 0,
+          pageText: book[0].page[prev.pageNum+1].body,
+          sentences: newSentences,
+          pageNum: prev.pageNum+1,
+        });
+      });
+      scrollRef.current.scrollTo({y: 0}); // scroll back to top
     }
-    setIsPlaying(!isPlaying); // because updating state has a lag, we do it at the end to ensure lag doesn't affect anything
-  };
+  }
 
-  useEffect(() => {
-    if (sliderValue === maxSliderValue) {
-      clearInterval(intervalObj);
-      setIsPlaying(false);
+  // need to re-add listener because eventlistener depends on current state, so cannot pass empty dependency 
+  // so this useeffect will run multiple times, but we technically only want the eventlistener to only be added once, hence the removal + add back
+  useEffect(() => { 
+    Tts.removeAllListeners('tts-finish');
+    Tts.addEventListener('tts-finish', advance);
+  }, [page]);
+
+  // function to handle play/pause
+  const onPlayPressed = () => {
+    if (!isPlaying) { // if not previously playing, play audio
+      Tts.speak(page.sentences[page.sentenceNum].trim());
+    } else { // if previously playing audio, stop audio
+      Tts.stop();
     }
-  }, [sliderValue]);
+    setIsPlaying(!isPlaying); // do this at the end because updating state has a lag
+  }
 
+  // function for stop button. stops TTS and reset progress in page to the first sentence
+  const onStopPressed = () => { 
+    setIsPlaying(false);
+    setPage(prev => ({
+      ...prev,
+      sentenceNum: 0,
+    }));
+    Tts.stop();
+    scrollRef.current.scrollTo({y: 0}); // scroll back to top
+  }
+
+  // function to switch to a bookmark's page
+  const onBookmarkPressed = (page) => {
+    const currPlaying = isPlaying;
+    if (currPlaying) {
+      Tts.stop(); // stop current playback
+    }
+    setPage(prev => { // go to chosen page
+      const newSentences = book[0].page[page].body.match(sentenceRegex);
+      if (currPlaying) Tts.speak(newSentences[0].trim()); // play chosen page
+      return ({
+        ...prev,
+        sentenceNum: 0,
+        pageText: book[0].page[page].body,
+        sentences: newSentences,
+        pageNum: page,
+      })
+    });
+    scrollRef.current.scrollTo({y: 0}); // scroll back to top
+    panelRef.current.hide();
+  }
+
+  const addNewBookmark = (bookmarkName) => {
+    setBookmarks(prev => [...prev, {
+      _id: Math.floor(Math.random() * 1000), // generate temp random id to add to bookmark flatlist
+      name: bookmarkName,
+      page: page.pageNum
+    }]);
+    // addBookmark(userInfo.user.id, book._id, bookmarkName, page.pageNum)
+    // .then(() => {
+    //   setBookmarks(prev => [...prev, {
+    //     _id: Math.floor(Math.random() * 1000), // generate temp random id to add to bookmark flatlist
+    //     name: bookmarkName,
+    //     page: page.pageNum
+    //   }]);
+    // })
+    // .catch((err) => console.log(err));
+  }
+
+  const removeOldBookmark = (bookmarkID) => {
+    let index = 0;
+    for (let i=0; i < bookmarks.length; i++) {
+      if (bookmarks[i]._id === bookmarkID) {
+        index = i;
+        break;
+      }
+    }
+    // correct bookmarkID will always be found
+    setBookmarks([...bookmarks.slice(0, index), ...bookmarks.slice(index+1, bookmarks.length)]);
+    // removeBookmark(userInfo.user.id, book._id, bookmarkID)
+    // .then(() => {
+    //   let index = 0;
+    //   for (let i=0; i < bookmarks.length; i++) {
+    //     if (bookmark._id === bookmarkID) {
+    //       index = i;
+    //       break;
+    //     }
+    //   }
+    //   setBookmarks([...bookmarks.slice(0, index), ...bookmarks.slice(index+1, bookmarks.length)]);
+    // })
+  }
+  
   return (
     <SafeAreaView>
-      <StatusBar barStyle="dark-content" />
+      <StatusBar barStyle='dark-content' />
       <View style={styles.container}>
         <View style={styles.topBar}>
           <View style={styles.backIcon}>
-            <Pressable
-              //onPress={() => navigation.navigate('HomeScreen')}
-              onPress={() => navigation.goBack()}
-              android_ripple={{color: 'gray', borderless: true}}>
-              <FontAwesomeIcon
-                icon={faChevronCircleLeft}
-                size={36}
-                color={COLORS.offblack}
-                style={{margin: -1}}
-              />
+            <Pressable onPress={exitScreen} android_ripple={{color: 'gray', borderless: true}}>
+              <FontAwesomeIcon icon={faChevronCircleLeft} size={36} color={COLORS.offblack}/>
             </Pressable>
           </View>
           <Text style={FONTS.h1}>Title of Book</Text>
         </View>
-        <ScrollView style={styles.textReader}>
-          <Text style={styles.textStyle}>{sampleText}</Text>
+        <ScrollView style={styles.textReader} ref={scrollRef}>
+          <HighlightText 
+            style={styles.textStyle}
+            highlightStyle={{ backgroundColor: COLORS.saffron }}
+            searchWords={[page.sentences[page.sentenceNum].trim()]}
+            textToHighlight={page.pageText}
+          />
         </ScrollView>
         <View style={styles.bottomBar}>
-          <Text style={FONTS.h2}>Pg {currPageNum}</Text>
-          <Slider
-            style={styles.slider}
-            minimumValue={0}
-            maximumValue={maxSliderValue}
-            minimumTrackTintColor="#FFFFFF"
-            maximumTrackTintColor="#000000"
-            value={sliderValue}
-          />
-          <View style={styles.controlBar}>
-            <View style={styles.controlIcons}>
-              <Pressable
-                onPress={onPlayPressed}
-                android_ripple={{color: 'gray', borderless: true}}>
-                <FontAwesomeIcon
-                  icon={faPlayCircle}
-                  size={48}
-                  color={'black'}
-                />
+          <Text style={FONTS.h2}>Pg {page.pageNum}</Text>            
+          <View style={styles.controlBar}>   
+            <View style={styles.controlIcons}>   
+              <Pressable onPress={onPlayPressed} android_ripple={{color: 'gray', borderless: true}}>
+                  <FontAwesomeIcon icon={isPlaying ? faPauseCircle : faPlayCircle} size={48} color={'black'} />
               </Pressable>
             </View>
-            <View style={styles.controlIcons}>
-              {/* <Pressable onPress={() => console.log("pressed book options!")} android_ripple={{color: 'gray', borderless: true}}> */}
-              <Pressable
-                onPress={() => navigation.navigate('BookOptionsScreen')}
-                android_ripple={{color: 'gray', borderless: true}}>
-                <FontAwesomeIcon icon={faBookmark} size={42} color={'black'} />
+            <View style={styles.controlIcons}>   
+              <Pressable onPress={onStopPressed} android_ripple={{color: 'gray', borderless: true}}>
+                <FontAwesomeIcon icon={faStopCircle} size={48} color={'black'} />
               </Pressable>
             </View>
+            <View style={styles.controlIcons}>   
+              <Pressable onPress={() => panelRef.current.show()} android_ripple={{color: 'gray', borderless: true}}>
+                <FontAwesomeIcon icon={faBookmark} size={42} color={'black'}/>
+              </Pressable>
+            </View>                        
           </View>
         </View>
+        <SlidingUpPanel 
+          ref={panelRef}
+          draggableRange={{top: 0.65*windowHeight, bottom: 0}}
+          friction={0.50}
+        >
+          <Bookmarks bookmarks={bookmarks} onBookmarkPressed={onBookmarkPressed} addNewBookmark={addNewBookmark} removeOldBookmark={removeOldBookmark}/>
+        </SlidingUpPanel>
       </View>
     </SafeAreaView>
   );
 }
 
-const sampleText =
-  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum aliquet malesuada turpis quis ullamcorper.' +
-  'Donec pharetra dignissim massa id scelerisque. Nunc viverra volutpat sapien ut pulvinar. Pellentesque at luctus nibh,' +
-  ' nec vulputate erat. Cras suscipit nibh eget quam faucibus pharetra. Nullam id neque tempus, efficitur justo vitae, pharetra nisl.' +
-  ' Nam et augue sed ligula mollis aliquam consectetur id orci. Lorem ipsum dolor sit amet, consectetur adipiscing elit.' +
-  ' Curabitur eu blandit augue. Phasellus fringilla feugiat venenatis. Curabitur nec accumsan mauris. Aliquam pretium massa ' +
-  'vel nunc congue ultrices. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.' +
-  ' Pellentesque sed imperdiet massa. Suspendisse lacinia, nibh nec dictum facilisis, quam tellus posuere magna, eget rhoncus' +
-  ' enim lectus nec risus. Vestibulum sit amet sollicitudin lacus, in pulvinar dolor. Sed ullamcorper tellus et lacus vulputate,' +
-  ' sit amet dictum tortor placerat. Pellentesque mollis urna sit amet risus placerat porta. Cras mi odio, commodo a tellus eget,' +
-  ' consectetur gravida augue. Ut et dignissim enim. Sed viverra dui ipsum, nec blandit mauris viverra vitae. Sed metus quam,' +
-  ' vestibulum sed pulvinar quis, viverra in nunc. Sed mattis sollicitudin quam porta rutrum. Proin molestie, augue in maximus aliquam,' +
-  ' velit justo hendrerit mi, nec vehicula nunc libero sit amet sem. Aenean sed odio varius, scelerisque ex in, lacinia arcu.' +
-  ' Donec eu nisi ligula. Fusce congue, leo id volutpat dignissim, sem metus bibendum felis, in laoreet quam arcu at ante.' +
-  ' Morbi non risus ac elit aliquam aliquet. Nunc mi nisl, finibus nec elit et, vulputate porttitor quam. Nullam efficitur semper' +
-  ' sodales. Phasellus a elit nulla. Vivamus ullamcorper blandit tempor. Fusce bibendum nisl vel libero sagittis, eget elementum' +
-  ' nisl luctus. Quisque dictum velit ut tincidunt aliquam. Fusce vel sapien quis ex sagittis suscipit a id ligula. Etiam semper' +
-  ' sapien ante, id imperdiet nunc semper a.';
