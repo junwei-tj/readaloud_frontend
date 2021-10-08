418d3a3667d2458c60246605dd07e4e6bd6e758d
Jun Wei <tohjunwei.tj@gmail.com>
2021-10-08 13:28:08 +0800

    - removed @sanar/react-native-highlight-text, and replaced it with custom component TextReader - added cleanup listeners for PlayAudioScreen

---
+++ components/TextReader.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+// component that highlights a sentence within an array of sentences
+export default function TextReader({ text, currSentence, style, highlightStyle }) {
+  /**
+   * @param {array} text an array of sentences
+   * @param {number} currSentence the number of the sentence to highlight. starts from 1
+   * @param {object} style style to apply on the text
+   * @param {object} highlightStyle style to apply for the highlighted sentence
+   */
+
+  // after every sentence, need to add a space
+  // need a separate Text for adding a space to prevent it from being highlighted
+  return (
+    <Text style={style}>
+      {text.map((sentence, index) => {
+        return index+1 !== currSentence ? 
+          (
+            <Text key={index}>{sentence.trim()+" "}</Text>
+          ) : (
+            <Text key={index}>
+              <Text style={highlightStyle}>
+                {sentence.trim()}
+              </Text>
+              {" "}
+            </Text>
+          )
+      })}
+    </Text>
+  )
+}
